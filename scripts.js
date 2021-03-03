a8ec7355079e9c3d1a39d07c26dff58538ce8442
L3p1989 <phogan110@live.com>
2021-03-02 23:34:25 -0700

    made const for body element. created div.friend. insert div.friend into body for every friend

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 // Array of friends
 let friends = ["Irish", "Siacca", "Glenn", "Lyndz", "Necco"];
+//Grab body
+const body = document.body;
 //Grab button.friend-btn
 const friendBtn = document.getElementById('friendBtn')
 //Add click event listener to friendBtn
@@ -8,6 +10,12 @@ friendBtn.addEventListener('click', () => {
     for (i = 0; i < friends.length; i++) {
         // assigns friend to current position in friends array
         let friend = friends[i];
+        //creates div
+        const div = document.createElement('div');
+        //give div className "friend"
+        div.className = "friend"
+        //Insert div into body
+        body.appendChild(div);
         // creates loop that counts down from 99
         for (n = 99; n > 0; n--) {
             // assigns minusOne as current loop number - 1
